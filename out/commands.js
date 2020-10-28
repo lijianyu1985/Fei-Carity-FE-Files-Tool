afd45c2ff8252103196efa3a9e6cc2bd869c8a0b
znyaiw <znyaiw@163.com>
2020-10-28 14:12:20 +0800

    Update context menu

---
+++ out/commands.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.commandsMap = void 0;
+exports.commandsMap = new Map([
+    ['extension.carityEnvironment', { fileName: 'carity environment', resource: '' }],
+    ['extension.carityRouter', { fileName: 'carity router', resource: '' }],
+    ['extension.carityAdd', { fileName: 'carity add', resource: '' }],
+    ['extension.carityList', { fileName: 'carity list', resource: '' }],
+    ['extension.carityPanel', { fileName: 'carity panel', resource: '' }],
+    ['extension.carityPanelForm', { fileName: 'carity panel form', resource: '' }],
+    ['extension.carityProfile', { fileName: 'carity profile', resource: '' }],
+    ['extension.carityQuery', { fileName: 'carity query', resource: '' }],
+    ['extension.carityQueryRoot', { fileName: 'carity query root', resource: '' }],
+    ['extension.carityService', { fileName: 'carity service', resource: '' }],
+    ['extension.carityRootModule', { fileName: 'carity root module', resource: '' }]
+]);
+//# sourceMappingURL=commands.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { stringify } from 'querystring';
+import { ExtensionContext, commands } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import { commandsMap } from './commands';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "carity-files-tool" is now active!');
+export function activate(context: ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('carity-files-tool.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// 右键菜单注册
+	for (const [key, value] of commandsMap) {
+		const command = commands.registerCommand(key, args => { console.log(`Carity generate ${ key }`); });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from carity-files-tool!');
-	});
-
-	context.subscriptions.push(disposable);
+		context.subscriptions.push(command);
+	}
 }
 
 // this method is called when your extension is deactivated
